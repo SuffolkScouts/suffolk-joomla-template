2e3b36687038b7df53aa47537fff5e7a30e35b59
root <root@bowline.suffolkscouts.dev>
2021-05-22 17:01:09 +0100

    Added Favicons etc

--- index.php
+++ index.php
@@ -34,7 +34,6 @@ if ($task == "edit" || $layout == "form") {
 // Add Stylesheets
 $doc->addStyleSheet('https://cdn.jsdelivr.net/gh/scoutstrap/scoutstrap@0.1.1/dist/css/scoutstrap.min.css');
 
-//$doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/bootstrap.min.css');
 $doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/font-awesome.min.css');
 
 $doc->addStyleSheet($this->baseurl . '/templates/' . $this->template . '/css/template.css');
@@ -48,14 +47,6 @@ JHtml::_('jquery.framework');
 
 //Disable Joomla Bootstrap
 unset($doc->_scripts[JURI::root(true) . '/media/jui/js/bootstrap.min.js']);
-//Disable Joomla JQuery
-//unset($doc->_scripts['/media/jui/js/jquery.min.js']);
-//unset($doc->_scripts['/media/jui/js/jquery-migrate.min.js']);
-//unset($doc->_scripts['/media/jui/js/jquery-noconflict.js']);
-
-
-
-// $doc->addScript($this->baseurl . '/templates/' . $this->template . '/js/template.js');
 
 // Adjusting content width
 if ($this->countModules('sidebar-left') && $this->countModules('sidebar-right')) {
@@ -77,7 +68,16 @@ if ($this->countModules('sidebar-left') && $this->countModules('sidebar-right'))
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,400;1,600&display=swap" rel="stylesheet">
 
-        <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script> -->
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/apple-touch-icon.png') ?>">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/favicon-32x32.png') ?>">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/favicon-16x16.png') ?>">
+        <link rel="manifest" href="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/site.webmanifest') ?>">
+        <link rel="mask-icon" href="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/safari-pinned-tab.svg') ?>" color="#490499">
+        <link rel="shortcut icon" href="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/favicon.ico') ?>">
+        <meta name="msapplication-TileColor" content="#490499">
+        <meta name="msapplication-config" content="<?php echo($this->baseurl . '/templates/' . $this->template . '/images/favicon/browserconfig.xml') ?>">
+        <meta name="theme-color" content="#ffffff">
 
         <jdoc:include type="head" />
 
